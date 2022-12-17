802caabfff69b98f7ed8dd7075bfc19d4a5d85f2
blinnikk <ukri.blinnikka@tuni.fi>
2022-12-17 17:41:38 +0200

    added tests

--- test/camelcase.test.js
+++ test/camelcase.test.js
@@ -3,16 +3,28 @@ import assert from 'assert'
 import CamelCase from '../src/camelCase.js';
 
 describe('CamelCaseTest', function(){
-    it('Should remove all spaces and lowercase very first letter', function(){
+    it('Should remove all spaces and lowercase first letter and uppercase letters after a separator', function(){
         var result = CamelCase('Foo Bar')
         assert.equal(result, 'fooBar');
     });
-    it('Should remove all spaces and lowercase very first letter', function(){
+    it('Should remove all "-" signs and lowercase first letter and uppercase after a separator', function(){
         var result = CamelCase('--foo-bar--')
         assert.equal(result, 'fooBar');
     });
-    it('Should remove all spaces and lowercase very first letter', function(){
+    it('Should remove all spaces and lowercase first letter and uppercase after a separator', function(){
         var result = CamelCase('__FOO_BAR__')
         assert.equal(result, 'fooBar');
+    });
+    it('Empty string', function(){
+        var result = CamelCase('')
+        assert.equal(result, '');
+    });
+    it('one word -> should start with lowercase letter', function(){
+        var result = CamelCase('Word')
+        assert.equal(result, 'word');
+    });
+    it('Many spaces', function(){
+        var result = CamelCase('Word   Test   one   two ----  three')
+        assert.equal(result, 'wordTestOneTwoThree');
     });
 });
--- test/chunk.test.js
+++ test/chunk.test.js
@@ -3,11 +3,11 @@ import chunk from '../src/chunk.js';
 
 
 describe('chunk', function(){
-    it('Should split array to two element arrays when size is 2', function(){
+    it('Should split array to 2 + 2 arrays when size is 2', function(){
         let result = chunk(['a', 'b', 'c', 'd'], 2)
-        assert.deepEqual(result, obj1);
+        assert.deepEqual(result, [['a', 'b'], ['c', 'd']]);
     });
-    it('Should split array to 3 element arrays when size is 3', function(){
+    it('Should split array to 3 + 1  element arrays when size is 3', function(){
         let result = chunk(['a', 'b', 'c', 'd'], 3)
         assert.deepEqual(result, [['a', 'b', 'c'], ['d']]);
     });
@@ -18,6 +18,10 @@ describe('chunk', function(){
     it('Should be every elem in its owwn array when size is 1', function(){
         let result = chunk(['a', 'b', 'c', 'd'], 1)
         assert.deepEqual(result, [['a'], ['b'], ['c'], ['d']]);
+    });
+    it('Size 0. Should propably return empty list.', function(){
+        let result = chunk(['a', 'b', 'c', 'd'], 0)
+        assert.deepEqual(result, []);
     });
 
 });
