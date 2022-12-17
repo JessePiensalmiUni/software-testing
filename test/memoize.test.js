cb021042ddc22c87b517bf1be6862254bf82ed78
JessePiensalmiUni <119791411+JessePiensalmiUni@users.noreply.github.com>
2022-12-17 13:21:12 +0200

    Update memoize.test.js

--- test/memoize.test.js
+++ test/memoize.test.js
@@ -39,17 +39,7 @@
  *
  * // Replace `memoize.Cache`.
  * memoize.Cache = WeakMap
- *
-*import assert from 'assert';
-*import memoize from '../src/memoize.js';
-*
-*
- *describe('the memoize function', function(){
-  *  it('should return an array [1,2] when given a map and and a memoized values function', function(){
-  *      const values = memoize(values)
-  *      assert.deepEqual(values(object),[1,2])
-  *  });
-*/ })
+ */
 
 
 
